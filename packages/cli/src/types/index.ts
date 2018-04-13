e4a8ed5e5db93b89070b4dc522e4ddb4af80e395
Drew Colthorp <colthorp@atomicobject.com>
2018-04-13 13:09:48 -0400

    fix import

--- packages/cli/src/types/index.ts
+++ packages/cli/src/types/index.ts
@@ -1,4 +1,5 @@
 import { Package } from "read-pkg-up";
+import { Dependencies } from "./dependencies";
 
 /** statically assert that the second type is assignable to the first type. */
 export interface AssertAssignable<T, U extends T> {}
